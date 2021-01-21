c62488b9d2797ab805e1b21a3de9e2169707d89b
Pratik Jha <jha.pratik289@gmail.com>
2021-01-21 12:16:21 +0530

    completed registration side java script code

--- Test/Registration.js
+++ Test/Registration.js
@@ -1,14 +1,45 @@
-var arr = localStorage.getItem('admin');
-var items = JSON.parse(arr);
-arr=items
+var adminData = []
 
-function Register(){
-    window.location.href = "Registration.html";
-    // if (arr === null) {
-    //     document.getElementById("table1").style.display = "none";
-    //     window.location.href = "Registration.html";
-    // }
-    // else{
-    //     window.location.href = "Login.html";
-    // }
+function Register() {
+    var uname = document.getElementById('username').value;
+    var mail = document.getElementById('email').value;
+    var pwd = document.getElementById('password').value;
+    var cnfpwd = document.getElementById('confirm_password').value;
+    var city = document.getElementById('drp_city').value;
+    var state = document.getElementById('drp_State').value;
+
+
+    var Admin = {
+        Aname: uname,
+        Aemail: mail,
+        Apsd: pwd,
+        Acnfpsd: cnfpwd,
+        Acity: city,
+        Astate: state
+    };
+
+    //console.log(Admin)
+    if (localStorage.getItem('admin')) {
+        adminData = JSON.parse(localStorage.getItem('admin'));
+    }
+
+    adminData.push(Admin);
+    console.log(adminData);
+    localStorage.setItem("admin", JSON.stringify(adminData));
+    window.location="login.html";
+}
+
+function check() {
+    var arr = localStorage.getItem('admin');
+    var items = JSON.parse(arr);
+
+    arr = items;
+
+    if (arr === null) {
+        window.location ="Registration.html"
+    }
+    else{
+        alert("Admin Already Registerd.")
+        window.location="login.html"
+    }
 }
